a7a040a9be23e0c8832e7657b1a4ca98989266c8
shaweiguo <shaweiguo@yeah.net>
2023-10-09 16:02:03 +0800

    ...

--- src/data/downloader.py
+++ src/data/downloader.py
@@ -70,6 +70,26 @@ class Worker(threading.Thread):
             self._result_queue.put((request_ID, callable(*args, **kwds)))
 
 
+class ThreadWorker(threading.Thread):
+    def __init__(self, request_queue, result_queue, **kwds):
+        super().__init__(**kwds)
+        self.daemon = True
+        self._request_queue = request_queue
+        self._result_queue = result_queue
+        self.start()
+    
+    def run(self):
+        while True:
+            callable, args, kwds = self._request_queue.get()
+            self._result_queue.put((callable(*args, **kwds)))
+
+
+def get_urls(master_list_file_path, url_queue):
+    df = pd.ExcelFile(master_list_file_path).parse('List')
+    for url in df['urls'].values:
+        url_queue.put(url)
+
+
 def download(num, in_queue, out_queue):
     request_queue = queue.Queue()
     result_queue = queue.Queue()
@@ -166,10 +186,63 @@ def save_biom(num, in_queue, out_queue, dir):
         show_results()
 
 
-def get_urls(master_list_file_path, url_queue):
-    df = pd.ExcelFile(master_list_file_path).parse('List')
-    for url in df['urls'].values:
-        url_queue.put(url)
+def biom_download(in_queue, out_queue):
+    MASTER_LIST_FILE_PATH = '/home/sha/work/microbiome/data/meta/Master_List.xlsx'
+    BIOM_DIR = './tmp/biom'
+    DOWNLOAD_THREADS = 5
+    SAVE_THREADS = 3
+    URLS_QUEUE = queue.Queue()
+    BIOM_QUEUE = queue.Queue()
+    TSV_QUEUE = queue.Queue()
+    # CSV_QUEUE = queue.Queue()
+    get_urls(MASTER_LIST_FILE_PATH, URLS_QUEUE)
+    # logger.info(f"{urls}")
+    download(DOWNLOAD_THREADS, URLS_QUEUE, BIOM_QUEUE)
+    save_biom(SAVE_THREADS, BIOM_QUEUE, TSV_QUEUE, BIOM_DIR)
+    request_queue = queue.Queue()
+    result_queue = queue.Queue()
+
+    downloaders = [
+        Worker(request_queue, result_queue)
+        for _ in range(num)
+    ]
+    requests = {}
+
+    def pick_a_downloader():
+        return random.choice(downloaders)
+    
+    def get_file_name(url):
+        idx = url.rfind('/')
+        file_name = url[idx + 1:]
+        return file_name
+    
+    def get_biom(url):
+        import requests
+        req = requests.get(url)
+        out_queue.put({'url': url, 'biom': req.content})
+        # with open(os.path.join(biom_dir, get_file_name(url)), "wb") as f:
+        #     f.write(req.content)
+        return {"status": "ok", "url": url}
+    
+    def show_results():
+        while True:
+            try:
+                completed_id, results = result_queue.get_nowait()
+            except queue.Empty:
+                return
+            url = requests.pop(completed_id)
+            logger.info(f"Result {completed_id}: {url} -> {results}")
+    
+    while in_queue.qsize() > 0:
+        url = in_queue.get()
+        worker = pick_a_downloader()
+        request_id = worker.work(get_biom, url)
+        requests[request_id] = request_id
+        logger.info(f"Submitted request {request_id}: {request_id}")
+        show_results()
+    
+    while requests:
+        show_results()
 
 
 def main():
